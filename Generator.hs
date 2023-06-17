f74ed368d8069afe1b4aa3b1768e5c700ec11001
bruno.lemus <brunolemus553@gmail.com>
2023-06-17 18:18:57 -0300

    Fix generator

--- Generator.hs
+++ Generator.hs
@@ -9,11 +9,12 @@
 
 module Generator where
 
-import Syntax
--- se pueden agregar mas importaciones 
+-- se pueden agregar mas importaciones
 -- en caso de ser necesario
 
+import Control.Monad.State
 import Data.List
+import Syntax
 
 -- CODE GENERATOR
 
@@ -32,10 +33,9 @@ ex:
         #include <stdio.h>
         int main() {
         printf("%d\n",(23 + 4)); }
-    
+
     input:
     outut:
-
 
     input:
         double :: (Int) -> Int
@@ -65,7 +65,7 @@ ex:
         foo :: (Int,Int) -> Int
         foo(x,y) = (let x :: Int = y in x)
                     + (let y :: Int = x + y in y)
-                
+
         main = foo(2,4)
     outut:
         #include <stdio.h>
@@ -86,57 +86,109 @@ translateType :: Type -> String
 translateType TyInt = "int"
 translateType TyBool = "int"
 
-translateExpr :: Expr -> String
-translateExpr (Var name) = translateName name
-translateExpr (IntLit value) = show value
-translateExpr (BoolLit value) = show (if value then 1 else 0)
-translateExpr (Infix op left right) =
-    let
-        translatedOp = case op of
-            Add -> " + "
-            Sub -> " - "
-            Mult -> " * "
-            Div -> " / "
-            Eq -> " == "
-            NEq -> " != "
-            GTh -> " > "
-            LTh -> " < "
-            GEq -> " >= "
-            LEq -> " <= "
-    in
-        "(" ++ translateExpr left ++ translatedOp ++ translateExpr right ++ ")"
+-- translateExpr :: Expr -> Int -> (Int, String)
+-- translateExpr (Var name) count = (count, translateName name)
+-- translateExpr (IntLit value) count = (count, show value)
+-- translateExpr (BoolLit value) count = (count, show (if value then 1 else 0))
+-- translateExpr (Infix op left right) count =
+--   let translatedOp =
+--         ( case op of
+--             Add -> " + "
+--             Sub -> " - "
+--             Mult -> " * "
+--             Div -> " / "
+--             Eq -> " == "
+--             NEq -> " != "
+--             GTh -> " > "
+--             LTh -> " < "
+--             GEq -> " >= "
+--             LEq -> " <= "
+--         )
+--       (count1, translatedLeft) = translateExpr left count
+--       (count2, translatedRight) = translateExpr right count1
+--    in (count2, "(" ++ translatedLeft ++ translatedOp ++ translatedRight ++ ")")
+-- translateExpr (If condition thenExpr elseExpr) count =
+--   let (count1, translatedCondition) = translateExpr condition count
+--       (count2, translatedThen) = translateExpr thenExpr count1
+--       (count3, translatedElse) = translateExpr elseExpr count2
+--    in (count3, translatedCondition ++ "?" ++ translatedThen ++ ":" ++ translatedElse)
+-- translateExpr (Let (name, _type) value body) count =
+--   let letName = "_let" ++ show count
+--       (count1, translatedValue) = translateExpr value (count + 1)
+--       (count2, translatedBody) = translateExpr body count1
+--    in (count2, "int " ++ letName ++ "(" ++ translateType _type ++ " " ++ translateName name ++ "){\nreturn (" ++ translatedBody ++ "); };\n" ++ letName ++ "(" ++ translatedValue ++ ")")
+-- translateExpr (App name args) count =
+--   let translatedName = translateName name
+--       (count1, translatedArgs) = mapAccumL (flip translateExpr) count args
+--    in (count1, translatedName ++ "(" ++ intercalate "," translatedArgs ++ ")")
+
+translateOperator :: Expr -> String
+translateOperator (Infix op _ _) =
+  case op of
+    Add -> " + "
+    Sub -> " - "
+    Mult -> " * "
+    Div -> " / "
+    Eq -> "=="
+    NEq -> "!="
+    GTh -> ">"
+    LTh -> "<"
+    GEq -> ">="
+    LEq -> "<="
+
+translateExpr :: Expr -> State [String] String -- State (Los lets que tengo que hacer, el codigo que llama a los lets)
+translateExpr (Var name) = return $ translateName name
+translateExpr (IntLit value) = return $ show value
+translateExpr (BoolLit value) = return $ show (if value then 1 else 0)
+translateExpr (Infix op e1 e2) =
+  do
+    translatedLeft <- translateExpr e1
+    translatedRight <- translateExpr e2
+    let translatedOperator = translateOperator (Infix op e1 e2)
+    return ("(" ++ translatedLeft ++ translatedOperator ++ translatedRight ++ ")")
 translateExpr (If condition thenExpr elseExpr) =
-    "(" ++ translateExpr condition ++ "?" ++ translateExpr thenExpr ++ ":" ++ translateExpr elseExpr ++ ")"
-translateExpr (Let (name, _type) value body) =
-    let
-        translatedName = translateName name
-        translatedValue = translateExpr value
-        translatedBody = translateExpr body
-    in
-        "int " ++ translatedName ++ " = " ++ translatedValue ++ ";\n" ++ translatedBody
+  do
+    translatedCondition <- translateExpr condition
+    translatedThen <- translateExpr thenExpr
+    translatedElse <- translateExpr elseExpr
+    return (translatedCondition ++ "?" ++ translatedThen ++ ":" ++ translatedElse)
 translateExpr (App name args) =
-    let
-        translatedName = translateName name
-        translatedArgs = map translateExpr args
-    in
-        translatedName ++ "(" ++ intercalate "," translatedArgs ++ ")"
+  do
+    translatedArgs <- mapM translateExpr args
+    return (translateName name ++ "(" ++ intercalate "," translatedArgs ++ ")")
+translateExpr s@(Let _ e1 _) =
+  do
+    translatedExpr <- translateExpr e1
+    letNumber <- translateLet s
+    return ("_let" ++ show letNumber ++ "(" ++ translatedExpr ++ ")")
+
+translateLet :: Expr -> State [String] Int -- State (Los lets que tengo que hacer, el codigo que llama a los lets)
+translateLet (Let (varName, _type) expr body) =
+  do
+    letCount <- gets length
+    translatedBody <- translateExpr body
+    newCount <- gets length
+    let translatedVarName = translateName varName
+    let translatedType = translateType _type
+    reversedLets <- gets reverse
+    let diff = take (newCount - letCount) reversedLets
+    let translatedLet = case diff of
+          [] -> "int " ++ "_let" ++ show newCount ++ "(" ++ translatedType ++ " " ++ translatedVarName ++ "){\nreturn (" ++ translatedBody ++ "); };\n"
+          functions -> "int " ++ "_let" ++ show newCount ++ "(" ++ translatedType ++ " " ++ translatedVarName ++ "){\n" ++ concat functions ++ "return (" ++ translatedBody ++ "); };\n"
+    _ <- put (drop (newCount - letCount) reversedLets)
+    _ <- modify (\s -> s ++ [translatedLet])
+    return newCount
 
 translateFunction :: FunDef -> String
 translateFunction (FunDef (name, Sig params returnType) args body) =
-    let
-        translatedName = translateName name
-        translatedParams = zipWith (\ name _y -> translateName name) args params
-        translatedReturnType = translateType returnType
-        translatedBody = translateExpr body
-    in
-        translatedReturnType ++ " " ++ translatedName ++ "(" ++ intercalate "," translatedParams ++ ") {\n" ++ translatedBody ++ "};\n"
-
+  let translatedName = translateName name
+      translatedParams = zipWith (\name _y -> translateType _y ++ " " ++ translateName name) args params
+      translatedReturnType = translateType returnType
+      (translatedBody, localFunctions) = runState (translateExpr body) []
+   in translatedReturnType ++ " " ++ translatedName ++ "(" ++ intercalate "," translatedParams ++ "){\n" ++ concat localFunctions ++ "return (" ++ translatedBody ++ "); };\n"
 
 genProgram :: Program -> String
 genProgram (Program defs expr) =
-    let
-        translatedDefs = map translateFunction defs
-        translatedExpr = translateExpr expr
-    in
-        "#include <stdio.h>" ++ intercalate "\n" translatedDefs ++ "\nint main() {\nprintf(\"%d\\n\"," ++ translatedExpr ++ "); }\n"
-    
+  let translatedDefs = map translateFunction defs
+      (translatedExpr, mainLets) = runState (translateExpr expr) []
+   in "#include <stdio.h>\n" ++ concat translatedDefs ++ "int main() {\n" ++ concat mainLets ++ "printf(\"%d\\n\"," ++ translatedExpr ++ "); }\n"
